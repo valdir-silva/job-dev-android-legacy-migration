a08cb60dbedeca546978e7e361e331cff0c66e95
Valdir Oliveira <whiteofdark@outlook.com>
2025-10-19 14:29:28 -0300

    test: corrige teste unitário

--- app/src/test/java/com/goomer/ps/GsonParsingTest.java
+++ app/src/test/java/com/goomer/ps/GsonParsingTest.java
@@ -2,6 +2,8 @@ package com.goomer.ps;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.goomer.ps.domain.model.MenuItem;
+
 import org.junit.Test;
 
 import java.lang.reflect.Type;
@@ -13,16 +15,24 @@ public class GsonParsingTest {
 
     @Test
     public void parseMenuJson_shouldReturnItems() {
+        // Given
         String json = "[\n  {\"id\":1,\"name\":\"Item\",\"description\":\"Desc\",\"price\":10.5,\"imageUrl\":\"\"}\n]";
         Gson gson = new Gson();
         Type listType = new TypeToken<List<MenuItem>>(){}.getType();
+        
+        // When
         List<MenuItem> items = gson.fromJson(json, listType);
-        assertNotNull(items);
-        assertEquals(1, items.size());
+        
+        // Then
+        assertNotNull("Lista de itens não deve ser nula", items);
+        assertEquals("Deve ter exatamente 1 item", 1, items.size());
+        
         MenuItem item = items.get(0);
-        assertEquals(1, item.id);
-        assertEquals("Item", item.name);
-        assertEquals("Desc", item.description);
-        assertEquals(10.5, item.price, 0.001);
+        assertNotNull("Item não deve ser nulo", item);
+        assertEquals("ID deve ser 1", 1, item.id);
+        assertEquals("Nome deve ser 'Item'", "Item", item.name);
+        assertEquals("Descrição deve ser 'Desc'", "Desc", item.description);
+        assertEquals("Preço deve ser 10.5", 10.5, item.price, 0.001);
+        assertEquals("URL da imagem deve ser vazia", "", item.imageUrl);
     }
 }
